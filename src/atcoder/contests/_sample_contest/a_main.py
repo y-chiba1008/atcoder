e2d2f9414235d96d12547f73a93fb5cab29e6f9f
y-chiba1008 <118287721+y-chiba1008@users.noreply.github.com>
2026-05-05 10:09:45 +0000

    fix: mainロジックを修正

--- src/atcoder/contests/_sample_contest/a_main.py
+++ src/atcoder/contests/_sample_contest/a_main.py
@@ -1,4 +1,11 @@
-def main(input=input, print=print):
+from typing import Callable
+
+
+def main(input: Callable[[], str] = input, print: Callable[..., None] = print):
     print(input())
     print(input())
     print(input())
+
+
+if __name__ == '__main__':
+    main()
