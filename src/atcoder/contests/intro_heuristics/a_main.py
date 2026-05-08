fed18271f1b9a1bfc4dc138596add946b82dc0e2
y-chiba1008 <118287721+y-chiba1008@users.noreply.github.com>
2026-05-08 09:25:06 +0000

    修正中

--- src/atcoder/contests/intro_heuristics/a_main.py
+++ src/atcoder/contests/intro_heuristics/a_main.py
@@ -1,15 +1,14 @@
-import random
 from typing import Callable
 
 N_TYPE = 26
 
 
-def scores_of_day(day: int, last: list[int], c: list[int], s_d: list[int]) -> list[int]:
-    """タイプiを選んだとしたら、その日に得られる得点のリスト"""
-    losses = [c[type_idx] * (day - last[type_idx]) for type_idx in range(N_TYPE)]
-    total_loss = sum(losses)
-    scores = [s_d[i] - (total_loss - losses[i]) for i in range(N_TYPE)]
-    return scores
+def score_of_day(day, c, s_d, t_d, last):
+    gain = s_d[t_d - 1]
+    scores = [-cost * (day - last_day) for cost, last_day in zip(c, last)]
+    scores[t_d - 1] = gain
+    score = sum(scores)
+    return score
 
 
 def main(input: Callable[[], str] = input, print: Callable[..., None] = print):
@@ -20,13 +19,13 @@ def main(input: Callable[[], str] = input, print: Callable[..., None] = print):
     for _ in range(D):
         s.append([int(item) for item in input().split()])
 
-    # タイプ毎の最後に実施した日
+    days = range(1, D + 1)
     last = [0 for _ in range(N_TYPE)]
-
-    # 貪欲法
-    score_total = 0
-    for day, s_d in zip(range(1, D + 1), s):
-        scores = scores_of_day(day, last, c, s_d)
+    for day, s_d in zip(days, s):
+        scores = [
+            score_of_day(day, c, s_d, type_temp, last)
+            for type_temp in range(1, N_TYPE + 1)
+        ]
         max_score = max(scores)
         max_idx = scores.index(max_score)
         print(max_idx + 1)
--- src/atcoder/contests/intro_heuristics/b_main.py
+++ src/atcoder/contests/intro_heuristics/b_main.py
@@ -3,14 +3,11 @@ from typing import Callable
 N_TYPE = 26
 
 
-def score_of_day(
-    day: int, last: list[int], c: list[int], s_d: list[int], t_d: int
-) -> int:
-    i = t_d - 1
-    score = s_d[i]
-    last[i] = day
-    costs = [c[type_idx] * (day - last[type_idx]) for type_idx in range(N_TYPE)]
-    score -= sum(costs)
+def score_of_day(day, c, s_d, t_d, last):
+    gain = s_d[t_d - 1]
+    scores = [-cost * (day - last_day) for cost, last_day in zip(c, last)]
+    scores[t_d - 1] = gain
+    score = sum(scores)
     return score
 
 
@@ -25,13 +22,13 @@ def main(input: Callable[[], str] = input, print: Callable[..., None] = print):
     for _ in range(D):
         t.append(int(input()))
 
-    # タイプ毎の最後に実施した日
+    score = 0
+    days = range(1, D + 1)
     last = [0 for _ in range(N_TYPE)]
-
-    score_total = 0
-    for day, s_d, t_d in zip(range(1, D + 1), s, t):
-        score_total += score_of_day(day, last, c, s_d, t_d)
-        print(score_total)
+    for day, s_d, t_d in zip(days, s, t):
+        score += score_of_day(day, c, s_d, t_d, last)
+        last[t_d - 1] = day
+        print(score)
 
 
 if __name__ == '__main__':
